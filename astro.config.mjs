7a12978e7c533047f26abff0190ab49fc98e1bed
Maksim Popov <m.popov@bpcbt.com>
2024-10-21 20:42:36 +0300

    Belka Pay

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://myupopov.github.io',
+  base: 'belkapay',
+  integrations: [tailwind()],
+  // build: {
+  //   format: 'file',
+  // },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/variables.scss";`,
+        },
+      },
+    }
+  },
+
+
+});
